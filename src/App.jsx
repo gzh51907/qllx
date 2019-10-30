45cb9a26adf07d671c952c51fe0eb38ece5b8b1a
Haowei404 <745658693@qq.com>
2019-10-30 11:52:32 +0800

    zwh

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { tsConstructorType, appendToMemberExpression } from '@babel/types';
 import { Menu, Icon } from 'antd';
 import { Route, Switch,Redirect} from 'react-router-dom'
 import Home from './pages/Home/Home.jsx';
+import './App.css'
 // import Destination from './pages/destination'
 // import Message from './pages/message'
 // import Discover from './pages/discover'
@@ -20,28 +21,33 @@ class App extends React.Component {
           <div className="center-wrap JS_ftTab">
             <a href="/" className="ft-btn-out curr">
               <div className="ft-btn-inner">
-                <Icon type="home" className="icon-ft b1"></Icon>
+                <Icon type="home" className="icon-ft b1" style={{fontSize:25}}></Icon>
+                <p className='bottom-text'>首页</p>
               </div>
             </a>
             <a href="/lineInfor/" className="ft-btn-out">
               <div className="ft-btn-inner">
-                <Icon type="global" className="icon-ft b2"></Icon>
+                <Icon type="global" className="icon-ft b2" style={{fontSize:25}}></Icon>
+                <p className='bottom-text'>目的地</p>
               </div>
             </a>
             <a href="/consult/" className="ft-btn-out">
               <div className="ft-btn-inner">
-                <Icon type="message" className="icon-ft b4"></Icon>
+                <Icon type="message" className="icon-ft b4" style={{fontSize:25}}></Icon>
+                <p className='bottom-text'>咨询</p>
               </div>
             </a>
             <a href="/discover/" className="ft-btn-out">
               <div className="ft-btn-inner">
-                <Icon type="eye" className="icon-ft b3"></Icon>
+                <Icon type="eye" className="icon-ft b3" style={{fontSize:25}}></Icon>
+                <p className='bottom-text'>发现</p>
               </div>
             </a>
 
             <a href="/mine/" className="ft-btn-out">
               <div className="ft-btn-inner">
-                <Icon type="user" className="icon-ft b5"></Icon>
+                <Icon type="user" className="icon-ft b5" style={{fontSize:25}}></Icon>
+                <p className='bottom-text'>我的</p>
               </div>
             </a>
           </div>
